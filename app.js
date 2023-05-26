b2ae0d0f6af287f96115bf0145c4ffe3b4458cea
Victor Martinez Jr <victormtzcodes@gmail.com>
2023-05-25 22:10:17 -0400

    added body overlay and logic to show/hide when mobile nav menu is open/closed

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 "use strict";
 
 const app = () => {
-  /**** Nav ****/
+  const bodyOverlay = document.querySelector(".overlay");
   const mobileNavLinks = document.querySelector(".nav__links");
   const mobileNavMenuIcon = document.querySelector(".nav__menuicon");
   const mobileNavMenuCloseIcon = document.querySelector(
     ".nav__menuicon--close"
   );
 
+  /** show/hide body overlay **/
+  const showBodyOverlay = () => {
+    bodyOverlay.classList.add("overlay__visible");
+  };
+  const hideBodyOverlay = () => {
+    bodyOverlay.classList.remove("overlay__visible");
+  };
+
   /** open/close links functions **/
   const openMobileNavLinks = () => {
     mobileNavLinks.classList.add("open");
+    showBodyOverlay();
   };
   const closeMobileNavLinks = () => {
     mobileNavLinks.classList.remove("open");
+    hideBodyOverlay();
   };
 
   /** event listeners **/
   mobileNavMenuIcon.addEventListener("click", openMobileNavLinks);
   mobileNavMenuCloseIcon.addEventListener("click", closeMobileNavLinks);
-
-  /**** Nav end ****/
 };
 
 app();
